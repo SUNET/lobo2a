f01c6d747fb4bebfb421507bc3be935487a51168
Leif Johansson <leifj@sunet.se>
2014-07-07 15:14:36 +0200

    minimal info

---
+++ README.md
@@ -0,0 +1,6 @@
+
+Lobo2 ARIA driver
+=================
+
+A simple application demonstrating the lobo2 API
+
---
+++ start-aria.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+aria2c --enable-rpc --seed-ratio=0.0 --force-save=true --allow-overwrite=true --check-integrity=true
